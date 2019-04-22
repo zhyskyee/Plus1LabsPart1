115d4c26d19b3cb6b8e50efd1dd0d75b7d21f008
Manan <patel.mana@husky.neu.edu>
2019-04-22 10:24:47 -0500

    cors allowed all

--- backend-time/src/main/java/com/example/backendtime/TimeService.java
+++ backend-time/src/main/java/com/example/backendtime/TimeService.java
@@ -1,11 +1,13 @@
 package com.example.backendtime;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@CrossOrigin(allowCredentials = "true",origins = "*")
 public class TimeService {
 
     @GetMapping("/api/time")
