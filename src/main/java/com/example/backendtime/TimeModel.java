9154f2b0720c50c508c560b361f412f0ba533648
Manan <patel.mana@husky.neu.edu>
2019-04-22 11:16:33 -0500

    Added date updated model

---
+++ src/main/java/com/example/backendtime/TimeModel.java
@@ -0,0 +1,62 @@
+package com.example.backendtime;
+
+import java.util.Date;
+
+public class TimeModel {
+
+    private int hours;
+    private int mins;
+    private int secs;
+    private int month;
+    private int year;
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    private int day;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMins() {
+        return mins;
+    }
+
+    public void setMins(int mins) {
+        this.mins = mins;
+    }
+
+    public int getSecs() {
+        return secs;
+    }
+
+    public void setSecs(int secs) {
+        this.secs = secs;
+    }
+}
--- src/main/java/com/example/backendtime/TimeService.java
+++ src/main/java/com/example/backendtime/TimeService.java
@@ -4,14 +4,25 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 @RestController
 @CrossOrigin(allowCredentials = "true",origins = "*")
 public class TimeService {
 
     @GetMapping("/api/time")
-    public long getCurrentTime(){
-        return new Date().getTime();
+    public TimeModel getCurrentTime(){
+        Date toSend = new Date();
+        TimeModel currentTime =new TimeModel();
+        currentTime.setHours(toSend.getHours());
+        currentTime.setMins(toSend.getMinutes());
+        currentTime.setSecs(toSend.getSeconds());
+        GregorianCalendar c = new GregorianCalendar();
+        currentTime.setMonth(c.get(Calendar.MONTH));
+        currentTime.setDay(c.get(Calendar.DAY_OF_MONTH));
+        currentTime.setYear(c.get(Calendar.YEAR));
+        return currentTime;
     }
 }
