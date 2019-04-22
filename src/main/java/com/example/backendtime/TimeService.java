39a72619b3844844a47407aefa2bad4009692247
Manan <patel.mana@husky.neu.edu>
2019-04-22 12:12:03 -0500

    Removed Time Zone bug set the time zone to CDT

--- src/main/java/com/example/backendtime/TimeService.java
+++ src/main/java/com/example/backendtime/TimeService.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 @RestController
 @CrossOrigin(allowCredentials = "true",origins = "*")
@@ -15,8 +16,10 @@ public class TimeService {
     @GetMapping("/api/time")
     public TimeModel getCurrentTime(){
         TimeModel currentTime =new TimeModel();
-        GregorianCalendar c = new GregorianCalendar();
-        currentTime.setHours(c.get(Calendar.HOUR));
+        Calendar c = new GregorianCalendar();
+        TimeZone tz = TimeZone.getTimeZone("US/Central");
+        c.setTimeZone(tz);
+        currentTime.setHours(c.get(Calendar.HOUR_OF_DAY));
         currentTime.setMins(c.get(Calendar.MINUTE));
         currentTime.setSecs(c.get(Calendar.SECOND));
         currentTime.setMonth(c.get(Calendar.MONTH));
